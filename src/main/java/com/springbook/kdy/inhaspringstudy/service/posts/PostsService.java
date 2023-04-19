108f3cbfed49dd6813f65177d5108cd0e7736819
DAY <ekdud_____@naver.com>
2023-04-19 10:44:24 +0900

    Revert "Feat: "4장: 머스테치로 화면 구성하기""
    This reverts commit b301ef673c0971ba2d1b91b036eb52ce69ec1511.

--- src/main/java/com/springbook/kdy/inhaspringstudy/service/posts/PostsService.java
+++ src/main/java/com/springbook/kdy/inhaspringstudy/service/posts/PostsService.java
@@ -2,7 +2,6 @@ package com.springbook.kdy.inhaspringstudy.service.posts;
 
 import com.springbook.kdy.inhaspringstudy.domain.posts.Posts;
 import com.springbook.kdy.inhaspringstudy.domain.posts.PostsRepository;
-import com.springbook.kdy.inhaspringstudy.web.dto.PostsListResponseDto;
 import com.springbook.kdy.inhaspringstudy.web.dto.PostsResponseDto;
 import com.springbook.kdy.inhaspringstudy.web.dto.PostsSaveRequestDto;
 import com.springbook.kdy.inhaspringstudy.web.dto.PostsUpdateRequestDto;
@@ -10,14 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
-
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
@@ -40,20 +35,4 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
-
-    @Transactional(readOnly = true)
-    public List<PostsListResponseDto> findAllDesc(){
-        return postsRepository.findAllDesc().stream()
-                .map(PostsListResponseDto::new)
-                .collect(Collectors.toList());
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없었습니다. id=" + id));
-
-        postsRepository.delete(posts);
-    }
 }
-
